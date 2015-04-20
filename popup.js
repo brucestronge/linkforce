e82ee69cd3eb3085464eef612493ad288f9bc51c
Bruce Stronge <bruce@netengine.com.au>
2015-04-20 15:10:40 +1000

    fixes some erros on popup code

--- popup.js
+++ popup.js
@@ -48,58 +48,55 @@ function getCurrentTabUrl(callback) {
 }
 
 // linkedin
-function copy(
+function copy() {
 
-$(' .full-name').html()
-var fullName = $(' .full-name').html()
-fullName.split(' ')
-firstName = fullName.split(' ')[0]
-lastName = fullName.split(' ')[1]
+	$(' .full-name').html()
+	var fullName = $(' .full-name').html()
+	fullName.split(' ')
+	firstName = fullName.split(' ')[0]
+	lastName = fullName.split(' ')[1]
 
-$(' .title').html()
-var title = $(' .title').html()
+	$(' .title').html()
+	var title = $(' .title').html()
 
-var data = { firstName: firstName, lastName: lastName, title: title }
+	var data = { firstName: firstName, lastName: lastName, title: title }
 
-json = JSON.stringify(data)
+	json = JSON.stringify(data)
 
-)
-
-{
   return json;
 }
 
 copy() // => "{"firstName": "lastName": "title"}""
 
 
-// salesforce
-function paste(
+// // salesforce
+// function paste() {
 
-data = JSON.parse(json)
+// 	data = JSON.parse(json)
 
-$('#name_firstlea2').value
-firstName = $('#name_firstlea2').value
+// 	$('#name_firstlea2').value
+// 	firstName = $('#name_firstlea2').value
 
-$('#name_lastlea2').value
-lastName = $('#name_lastlea2').value
+// 	$('#name_lastlea2').value
+// 	lastName = $('#name_lastlea2').value
 
-$('#lea3').value
-title = $('#lea3').value
+// 	$('#lea3').value
+// 	title = $('#lea3').value
 
-	)
+// }
 
-paste('{"firstName": "lastName": "title"}')
+// paste('{"firstName": "lastName": "title"}')
 
-function eventFire(el, etype){
-  if (el.fireEvent) {
-    el.fireEvent('on' + etype);
-  } else {
-    var evObj = document.createEvent('Events');
-    evObj.initEvent(etype, true, false);
-    el.dispatchEvent(evObj);
-  }
-}
+// function eventFire(el, etype){
+//   if (el.fireEvent) {
+//     el.fireEvent('on' + etype);
+//   } else {
+//     var evObj = document.createEvent('Events');
+//     evObj.initEvent(etype, true, false);
+//     el.dispatchEvent(evObj);
+//   }
+// }
   
-  )
 
-eventFire($('.btn'), 'click')
+// eventFire($('.btn'), 'click')
+
